0aebad23c9d9ea63b9aa753a3add40bda9f9834a
Rachit Chandawar <130487757+rancho2274@users.noreply.github.com>
2024-08-03 20:17:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import torch
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 # Load the saved model and tokenizer
-model = T5ForConditionalGeneration.from_pretrained('../final prototype H4C')
-tokenizer = T5Tokenizer.from_pretrained('../final prototype H4C')
+model = T5ForConditionalGeneration.from_pretrained('./static')
+tokenizer = T5Tokenizer.from_pretrained('./static')
 
 # Ensure the model is in evaluation mode
 model.eval()
